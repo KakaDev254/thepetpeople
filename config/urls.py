abfe3774c0bbe2b755c20ead1edf647faebfe937
Enock <oswereenock@gmail.com>
2025-12-22 00:54:23 +0300

    fixed url

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -11,7 +9,3 @@ urlpatterns = [
     path('gallery/', include('gallery.urls')),
     path('shop/', include('shop.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
