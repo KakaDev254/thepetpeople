24566a4c74e456ca0f9e254b1c4bcfe0d6454301
Enock <oswereenock@gmail.com>
2026-04-29 20:20:16 +0300

    edited home page

--- core/views.py
+++ core/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.utils import timezone
 from .forms import BookingForm
 from .models import SERVICE_FEES
+from events.models import Event
 from blog.models import Post
 
 def home(request):
+    today = timezone.now().date()
+
     recent_posts = (
         Post.objects
         .filter(published=True)
         .order_by('-created_at')[:3]
     )
 
+    past_events = (
+        Event.objects
+        .filter(date__lt=today, is_published=True)
+        .order_by('-date')[:2]   # limit for homepage
+    )
+
     return render(request, "core/home.html", {
-        "recent_posts": recent_posts
+        "recent_posts": recent_posts,
+        "past_events": past_events
     })
 
 
